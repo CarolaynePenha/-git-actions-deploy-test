972b3bafa64e6be54ee3b370e6fbbc864bac055e
Carolayne <carolaynepenha@gmail.com>
2024-04-25 15:34:10 -0300

    deploy-test

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ import express from "express";
 const app = express();
 app.get("/", (req, res) => {
   res.send("Olá, T5!");
+  console.log("Teste");
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is up and running on port ${port}`);
-})
+});
